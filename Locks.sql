621399248140feceff1544a6a5843d6742416593
Fabio Fumio Wada <39932036+FabioFumioWada@users.noreply.github.com>
2021-08-13 16:59:45 -0300

    Create Locks.sql

---
+++ Locks.sql
@@ -0,0 +1,10 @@
+select sid,
+       serial#,
+       status,
+       username,
+       osuser,
+       program,  
+       blocking_session blocking,
+       event  
+       from v$session  
+       where blocking_session is not null; 
