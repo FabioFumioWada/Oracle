ae7759353438a48ef626b54cbbd02b07c3f29008
Fabio Fumio Wada <39932036+FabioFumioWada@users.noreply.github.com>
2021-08-13 16:47:08 -0300

    Create Select_*_Campos.sql

---
+++ Select_*_Campos.sql
@@ -0,0 +1,10 @@
+SELECT  X.*,
+               5 AS CODCOLIGADA
+FROM ZIMPFUNCIONARIOS_MATER X
+
+SELECT  5 AS CODCOLIGADA,
+              X.*
+FROM ZIMPFUNCIONARIOS_MATER X
+
+
+Sempre apelidar a tabela, para que esta forma de SELECT + campos adicionais funcione
